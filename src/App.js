61701d2b31824a1077349170e2e42a7be1fd74b2
Mike <szekleym@gmail.com>
2018-12-19 11:27:51 -0700

    fetch and display of data breakout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-//get the React library from the dependencies
 import React, { Component } from 'react';
-//get the loader from the npm package
 import Loader from 'react-loader-spinner'
 import './App.css';
 
-//instantiating the class that extends the React Component 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: ""
+    }
+  }
+
+  getData = () => {
+    return fetch('https://swapi.co/api/films/')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({data: data.results})
+      return data
+    })
+    .then(this.getMoveTitles)
+    .catch(err => console.error(err))
+  }
+
+  getMoveTitles = () => {
+    let title = this.state.data.map((moveTitle, index) => <p key={index}>{moveTitle.title}</p>)
+    return title
+  }
+  
+  componentDidMount() {
+    this.getData()
+  }
 
   render() {
-
-    //what we are returning in the render function
     return (
       <div className="App">
-        <h1>I find your lack of fetch disturbing..</h1>
-        <Loader type="Grid" color="red" height={80} width={80} />
+        <div>
+          {this.state.data ? 
+          <div>
+            <h1>Move Titles</h1>
+            {this.getMoveTitles()}
+          </div> : 
+          <div>
+            <h1>I find your lack of fetch disturbing..</h1>
+            <Loader type="Grid" color="red" height={80} width={80} />
+          </div>        
+          }
+        </div>
       </div>
-    );
+    )
   }
 }
 
